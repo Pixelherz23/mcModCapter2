2842db51d465234543bb1da99c6dc1a0b9984b70
Pixelherz23 <64771916+Pixelherz23@users.noreply.github.com>
2020-10-12 20:56:50 +0200

    init CustomSword class

---
+++ src/main/java/com/example/examplemod/CustomSword.java
@@ -0,0 +1,10 @@
+package com.example.examplemod;
+
+import net.minecraft.item.ItemSword;
+
+public class CustomSword extends ItemSword {
+
+    public CustomSword(ToolMaterial material) {
+        super(material);
+    }
+}
