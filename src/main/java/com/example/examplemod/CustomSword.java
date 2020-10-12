384fd72d6861dac82aa6f2a6f942a8132dfd90b7
Pixelherz23 <64771916+Pixelherz23@users.noreply.github.com>
2020-10-12 20:59:10 +0200

    define ctor of Sword class

--- src/main/java/com/example/examplemod/CustomSword.java
+++ src/main/java/com/example/examplemod/CustomSword.java
@@ -1,10 +1,15 @@
 package com.example.examplemod;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemSword;
 
 public class CustomSword extends ItemSword {
 
     public CustomSword(ToolMaterial material) {
         super(material);
+        this.setCreativeTab(CreativeTabs.COMBAT);
+        this.setRegistryName("my_sword");
+        this.setUnlocalizedName("my_sword");
+
     }
 }
