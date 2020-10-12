19971279e200bf35bcff43518751d4dd5c5a32d0
Pixelherz23 <64771916+Pixelherz23@users.noreply.github.com>
2020-10-12 21:11:10 +0200

    implemented function to register sword for MC

--- src/main/java/com/example/examplemod/CommonProxy.java
+++ src/main/java/com/example/examplemod/CommonProxy.java
@@ -1,4 +1,20 @@
 package com.example.examplemod;
 
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+//todo check the javadoc to this annotation
+//it will load the mod into minecraft
+@Mod.EventBusSubscriber
 public class CommonProxy {
+
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<Item> event){
+        //bad code. Class VAriable!!
+        event.getRegistry().registerAll(ExampleMod.sword);
+
+
+    }
 }
