f15e2a06cc90553107d4875a84fa5c708baad627
Pixelherz23 <64771916+Pixelherz23@users.noreply.github.com>
2020-10-13 10:12:28 +0200

    working on image loading

--- src/main/java/com/example/examplemod/CommonProxy.java
+++ src/main/java/com/example/examplemod/CommonProxy.java
@@ -1,6 +1,8 @@
 package com.example.examplemod;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -9,10 +11,16 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 //it will load the mod into minecraft
 @Mod.EventBusSubscriber
 public class CommonProxy {
+    //register the sword image
+    public static void registerRender(Item item){
+        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
+        //bad code class var
+        registerRender(ExampleMod.sword);
+    }
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event){
-        //bad code. Class VAriable!!
+        //bad code. Class Variable!!
         event.getRegistry().registerAll(ExampleMod.sword);
 
 
